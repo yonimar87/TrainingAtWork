2b42e7c22556c3fe9c9cf11cab55634e5b3db6b8
Yoni Marlow <yonathanmarlow@hotmail.com>
2021-03-22 09:50:11 +1100

    updated menu-items with navifation withRouter HOC

--- Udemy-ZtoM/crwn-clothing/src/App.js
+++ Udemy-ZtoM/crwn-clothing/src/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import './App.css';
+import {Switch, Route} from 'react-router-dom'; // switch doesn't render anything after it matches the path.
 
 import HomePage from './pages/homepage/homepage.component'
 
 function App() {
   return (
     <div>
-      <HomePage />
+      <Switch >
+        <Route exact path='/' component={HomePage} />
+      </Switch>
     </div>
   );
 }
