5db0f47c5ab6e866888c894c109fda77e719fd45
Yoni Marlow <yonathanmarlow@hotmail.com>
2021-03-22 15:09:46 +1100

    auth on firebase

--- Udemy-ZtoM/crwn-clothing/src/App.js
+++ Udemy-ZtoM/crwn-clothing/src/App.js
@@ -1,23 +1,48 @@
-import React from 'react';
-import './App.css';
-import {Switch, Route} from 'react-router-dom'; // switch doesn't render anything after it matches the path.
+import React from "react";
+import "./App.css";
+import { Switch, Route } from "react-router-dom"; // switch doesn't render anything after it matches the path.
+import { auth } from "./firebase/firebase.utils";
 
-import HomePage from './pages/homepage/homepage.component'
-import ShopPage from './pages/shop/shop.component'
-import Header from './components/header/header.component'
-import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component'
+import HomePage from "./pages/homepage/homepage.component";
+import ShopPage from "./pages/shop/shop.component";
+import Header from "./components/header/header.component";
+import SignInAndSignUpPage from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component";
 
-function App() {
-  return (
-    <div>
-      <Header />
-      <Switch >
-        <Route exact path='/' component={HomePage} />
-        <Route path='/shop' component={ShopPage} />
-        <Route path='/signin' component={SignInAndSignUpPage} />
-      </Switch>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      currentUser: null,
+    };
+  }
+
+  unsubscribeFromAuth = null;
+
+  componentDidMount() {
+    auth.onAuthStateChanged((user) => {
+      this.setState({ currentUser: user });
+
+      console.log(user);
+    });
+  }
+
+  componentWillUnmount() {
+    this.unsubscribeFromAuth();
+  }
+
+  render() {
+    return (
+      <div>
+        <Header />
+        <Switch>
+          <Route exact path="/" component={HomePage} />
+          <Route path="/shop" component={ShopPage} />
+          <Route path="/signin" component={SignInAndSignUpPage} />
+        </Switch>
+      </div>
+    );
+  }
 }
 
 export default App;
