9f305aadc16974a0a0b1fe11afe37b562a9b0038
Yoni Marlow <yonathanmarlow@hotmail.com>
2021-03-22 11:42:30 +1100

    git updating app with shop page, collection preview component and collection item component

--- Udemy-ZtoM/crwn-clothing/src/App.js
+++ Udemy-ZtoM/crwn-clothing/src/App.js
@@ -3,12 +3,14 @@ import './App.css';
 import {Switch, Route} from 'react-router-dom'; // switch doesn't render anything after it matches the path.
 
 import HomePage from './pages/homepage/homepage.component'
+import ShopPage from './pages/shop/shop.component.jsx'
 
 function App() {
   return (
     <div>
       <Switch >
         <Route exact path='/' component={HomePage} />
+        <Route path='/shop' component={ShopPage} />
       </Switch>
     </div>
   );
