fbff982f3cfa5ad65750f3a48becb5abde58e208
Yoni Marlow <yonathanmarlow@hotmail.com>
2021-03-23 11:25:23 +1100

    new commit

--- Udemy-ZtoM/crwn-clothing/src/components/sign-in/sign-in.component.jsx
+++ Udemy-ZtoM/crwn-clothing/src/components/sign-in/sign-in.component.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import FormInput from "../form-input/form-input.component";
 import CustomButton from "../custom-button/custom-button.component";
-import { signInWithGoogle } from "../../firebase/firebase.utils.js";
+import { auth, signInWithGoogle } from "../../firebase/firebase.utils.js";
 
 import "./sign-in.styles.scss";
 
@@ -15,10 +15,17 @@ class SignIn extends Component {
     };
   }
 
-  handleSubmit = (event) => {
+  handleSubmit = async (event) => {
     event.preventDefault();
 
-    this.setState({ email: "", password: "" });
+    const { email, password } = this.state;
+
+    try {
+      await auth.signInWithEmailAndPassword(email, password);
+      this.setState({ email: "", password: "" });
+    } catch (error) {
+      console.log(error);
+    }
   };
   handleChange = (event) => {
     const { value, name } = event.target;
@@ -44,7 +51,7 @@ class SignIn extends Component {
           <FormInput
             name="password"
             type="password"
-            value={this.state.email}
+            value={this.state.password}
             handleChange={this.handleChange}
             label="password"
             required
