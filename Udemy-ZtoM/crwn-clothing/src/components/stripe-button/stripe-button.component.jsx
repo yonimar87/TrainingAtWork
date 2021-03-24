8b37d8b2131c8539e3275126f3c8480b1eae92a8
Yoni Marlow <yonathanmarlow@hotmail.com>
2021-03-25 10:53:18 +1100

    updating stripe integration + favicon

---
+++ Udemy-ZtoM/crwn-clothing/src/components/stripe-button/stripe-button.component.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+
+import StripeCheckout from "react-stripe-checkout";
+
+const StripeCheckoutButton = ({ price }) => {
+  const priceForStripe = price * 100;
+  const publishableKey =
+    "pk_test_51IYg2rLSs8F4IJdFgaRymtAQTBZB51dDJpUFHjGeJuqo2TBHvavhzQUMKU2e9yGv0JlaZo3juO1TdFFPyVas7IET00U0Kt9gnO";
+
+  const onToken = (token) => {
+    console.log(token);
+    alert("payment successful");
+  };
+
+  return (
+    <StripeCheckout
+      label="Pay Now"
+      name="CRWN Clothing"
+      billingAddress
+      shippingAddress
+      image="https://svgshare.com/i/CUz.svg"
+      description={`Your total is $${price}`}
+      amount={priceForStripe}
+      panelLabel="Pay Now"
+      token={onToken}
+      stripeKey={publishableKey}
+    />
+  );
+};
+
+export default StripeCheckoutButton;
--- Udemy-ZtoM/crwn-clothing/src/pages/checkout/checkout.component.jsx
+++ Udemy-ZtoM/crwn-clothing/src/pages/checkout/checkout.component.jsx
@@ -6,7 +6,7 @@ import {
   selectCartTotal,
 } from "../../redux/cart/cart.selectors";
 import CheckoutItem from "../../components/checkout-item/checkout-item.component";
-
+import StripeCheckoutButton from "../../components/stripe-button/stripe-button.component";
 import "./checkout.styles.scss";
 
 const CheckoutPage = ({ cartItems, total }) => (
@@ -32,9 +32,14 @@ const CheckoutPage = ({ cartItems, total }) => (
     {cartItems.map((cartItem) => (
       <CheckoutItem key={cartItem.id} cartItem={cartItem} />
     ))}
-    <div className="total">
-      <span>TOTAL: ${total}</span>
+    <div className="total">TOTAL: ${total} </div>
+    <div className="test-warning">
+      {" "}
+      ** PLEASE USE THE FOLLOWING TEST CREDIT CARD FOR PAYMENT
+      <br />
+      4242 4242 4242 4242 - EXP 01/20 CVV: 123
     </div>
+    <StripeCheckoutButton price={total} />
   </div>
 );
 
