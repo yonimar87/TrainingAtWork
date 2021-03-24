67c56a4d9780fb6c360070a2378f158ebde4e862
Yoni Marlow <yonathanmarlow@hotmail.com>
2021-03-24 16:59:46 +1100

    more redux and reselect - added a collection page, added some collection component etc.

--- Udemy-ZtoM/crwn-clothing/src/pages/shop/shop.component.jsx
+++ Udemy-ZtoM/crwn-clothing/src/pages/shop/shop.component.jsx
@@ -1,27 +1,13 @@
-import React, { Component } from 'react'
-import SHOP_DATA from './shop.data.js'
-import CollectionPreview from '../../components/collection-preview/collection-preview.component'
+import React from "react";
+import CollectionsOverview from "../../components/collections-overview/collections-overview.component";
+import { Route } from "react-router-dom";
+import CollectionPage from "../collection/collection.component";
 
-class ShopPage extends Component {
+const ShopPage = ({ match }) => (
+  <div className="shop-page">
+    <Route exact path={`${match.path}`} component={CollectionsOverview} />
+    <Route path={`${match.path}/:collectionId`} component={CollectionPage} />
+  </div>
+);
 
-    constructor(props) {
-        super(props);
-
-        this.state = {
-            collections: SHOP_DATA
-        }
-    }
-
-    render() {
-        const {collections} = this.state
-        return (
-        <div className="shop-page">
-            {collections.map(({id, ...otherCollectionProps}) => (
-                <CollectionPreview key={id} {...otherCollectionProps} />
-            ))}
-        </div>
-        )
-    }
-}
-
-export default ShopPage
+export default ShopPage;
--- Udemy-ZtoM/crwn-clothing/src/redux/root-reducer.js
+++ Udemy-ZtoM/crwn-clothing/src/redux/root-reducer.js
@@ -1,9 +1,24 @@
 // this is the code that combines all states together //
 import { combineReducers } from "redux";
+import { persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage"; // this is local storage from browser
+// import sessionStorage from "redux-persist/lib/storage"; // this is session storage from browser
 import userReducer from "./user/user.reducer";
 import cartReducer from "./cart/cart.reducer";
+import directoryReducer from "./directory/directory.reducer";
+import shopReducer from "./shop/shop.reducer";
 
-export default combineReducers({
+const persistConfig = {
+  key: "root", // starting at the root for storage
+  storage,
+  whitelist: ["cart"],
+};
+
+const rootReducer = combineReducers({
   user: userReducer,
   cart: cartReducer,
+  directory: directoryReducer,
+  shop: shopReducer,
 });
+
+export default persistReducer(persistConfig, rootReducer);
