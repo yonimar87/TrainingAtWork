8c629910fd2b1d31bd998094e55349da84fb7c2d
Yoni Marlow <yonathanmarlow@hotmail.com>
2021-03-24 11:37:07 +1100

    added add item and removeitem functionality to the checkout item --- helped with process again

--- Udemy-ZtoM/crwn-clothing/src/redux/cart/cart.actions.js
+++ Udemy-ZtoM/crwn-clothing/src/redux/cart/cart.actions.js
@@ -8,3 +8,13 @@ export const addItem = (item) => ({
   type: CartActionTypes.ADD_ITEM,
   payload: item, // can be anything we want it to be but we want it to be the item we are adding
 });
+
+export const clearItemFromCart = (item) => ({
+  type: CartActionTypes.CLEAR_ITEM_FROM_CART,
+  payload: item,
+});
+
+export const removeItem = (item) => ({
+  type: CartActionTypes.REMOVE_ITEM,
+  payload: item,
+});
